4d33e11528604e13b429fdbbe28b4c18f16d9a03
Lefebvre <Lefebvre@LAPTOP-AGLMKE6G>
2020-04-18 14:12:13 +0200

    c moi

--- Index.Designer.cs
+++ Index.Designer.cs
@@ -52,11 +52,12 @@
             this.buttonClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
             this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonClose.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(179)))), ((int)(((byte)(133)))));
-            this.buttonClose.Location = new System.Drawing.Point(1016, 3);
+            this.buttonClose.Location = new System.Drawing.Point(1355, 4);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(53, 42);
+            this.buttonClose.Size = new System.Drawing.Size(71, 52);
             this.buttonClose.TabIndex = 0;
             this.buttonClose.Text = "X";
             this.buttonClose.UseVisualStyleBackColor = true;
@@ -65,11 +66,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(231)))), ((int)(((byte)(203)))));
-            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(382, 58);
+            this.label1.Size = new System.Drawing.Size(480, 69);
             this.label1.TabIndex = 1;
             this.label1.Text = "LA CONFIN\'APP";
             // 
@@ -80,9 +82,10 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(630, 132);
+            this.button2.Location = new System.Drawing.Point(495, 262);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(319, 62);
+            this.button2.Size = new System.Drawing.Size(425, 76);
             this.button2.TabIndex = 2;
             this.button2.Text = "Filmothèque";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -95,9 +98,10 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.Black;
-            this.button3.Location = new System.Drawing.Point(630, 297);
+            this.button3.Location = new System.Drawing.Point(840, 366);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(319, 62);
+            this.button3.Size = new System.Drawing.Size(425, 76);
             this.button3.TabIndex = 3;
             this.button3.Text = "TV Show Tracker";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -110,9 +114,10 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.Black;
-            this.button4.Location = new System.Drawing.Point(630, 213);
+            this.button4.Location = new System.Drawing.Point(840, 262);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(319, 62);
+            this.button4.Size = new System.Drawing.Size(425, 76);
             this.button4.TabIndex = 4;
             this.button4.Text = "Meme-othèque";
             this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -125,9 +130,10 @@
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.ForeColor = System.Drawing.Color.Black;
-            this.button5.Location = new System.Drawing.Point(54, 394);
+            this.button5.Location = new System.Drawing.Point(72, 485);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(319, 62);
+            this.button5.Size = new System.Drawing.Size(425, 76);
             this.button5.TabIndex = 5;
             this.button5.Text = "Morpion";
             this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -136,22 +142,24 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(179)))), ((int)(((byte)(133)))));
-            this.label2.Location = new System.Drawing.Point(647, 49);
+            this.label2.Location = new System.Drawing.Point(863, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(172, 17);
+            this.label2.Size = new System.Drawing.Size(195, 20);
             this.label2.TabIndex = 7;
             this.label2.Text = "Ajouter ici la date du jour";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(179)))), ((int)(((byte)(133)))));
-            this.label3.Location = new System.Drawing.Point(825, 49);
+            this.label3.Location = new System.Drawing.Point(1100, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(194, 17);
+            this.label3.Size = new System.Drawing.Size(220, 20);
             this.label3.TabIndex = 8;
             this.label3.Text = "Compteur jours confinement";
             // 
@@ -162,9 +170,10 @@
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button6.ForeColor = System.Drawing.Color.Black;
-            this.button6.Location = new System.Drawing.Point(630, 378);
+            this.button6.Location = new System.Drawing.Point(840, 465);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(319, 62);
+            this.button6.Size = new System.Drawing.Size(425, 76);
             this.button6.TabIndex = 9;
             this.button6.Text = "Todo List";
             this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -177,9 +186,10 @@
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button8.ForeColor = System.Drawing.Color.Black;
-            this.button8.Location = new System.Drawing.Point(54, 231);
+            this.button8.Location = new System.Drawing.Point(495, 178);
+            this.button8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(319, 62);
+            this.button8.Size = new System.Drawing.Size(425, 76);
             this.button8.TabIndex = 11;
             this.button8.Text = "Calculatrice";
             this.button8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -192,9 +202,10 @@
             this.buttonPasswordGenerator.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonPasswordGenerator.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonPasswordGenerator.ForeColor = System.Drawing.Color.Black;
-            this.buttonPasswordGenerator.Location = new System.Drawing.Point(54, 311);
+            this.buttonPasswordGenerator.Location = new System.Drawing.Point(72, 383);
+            this.buttonPasswordGenerator.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.buttonPasswordGenerator.Name = "buttonPasswordGenerator";
-            this.buttonPasswordGenerator.Size = new System.Drawing.Size(319, 62);
+            this.buttonPasswordGenerator.Size = new System.Drawing.Size(425, 76);
             this.buttonPasswordGenerator.TabIndex = 12;
             this.buttonPasswordGenerator.Text = "Password Generator";
             this.buttonPasswordGenerator.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -208,9 +219,10 @@
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("Segoe UI Semilight", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button7.ForeColor = System.Drawing.Color.Black;
-            this.button7.Location = new System.Drawing.Point(630, 455);
+            this.button7.Location = new System.Drawing.Point(840, 560);
+            this.button7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(319, 62);
+            this.button7.Size = new System.Drawing.Size(425, 76);
             this.button7.TabIndex = 13;
             this.button7.Text = "Meteo";
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -225,17 +237,19 @@
             this.panel1.Controls.Add(this.buttonClose);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1072, 93);
+            this.panel1.Size = new System.Drawing.Size(1429, 114);
             this.panel1.TabIndex = 14;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(179)))), ((int)(((byte)(133)))));
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 93);
+            this.panel2.Location = new System.Drawing.Point(0, 114);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1072, 14);
+            this.panel2.Size = new System.Drawing.Size(1429, 17);
             this.panel2.TabIndex = 15;
             // 
             // label4
@@ -243,18 +257,19 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(47, 145);
+            this.label4.Location = new System.Drawing.Point(63, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(251, 37);
+            this.label4.Size = new System.Drawing.Size(316, 46);
             this.label4.TabIndex = 9;
             this.label4.Text = "Idées apps à coder :";
             // 
             // Index
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
-            this.ClientSize = new System.Drawing.Size(1072, 580);
+            this.ClientSize = new System.Drawing.Size(1429, 714);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -267,6 +282,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Index";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
